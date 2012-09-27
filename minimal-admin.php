c3b21dddaf630b55835f447ce43ba17c3f647078
Brent Shepherd <thenbrent@gmail.com>
2012-09-27 10:15:20 +1000

    Move hooks into constructor & functions into class

--- minimal-admin.php
+++ minimal-admin.php
@@ -28,75 +28,78 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 	
 class Eleven_Minimal_Admin {
-	
-		function __construct(){
 
+	function __construct(){
 		// hide admin bar when viwing site
-		add_filter( 'show_admin_bar', '__return_false');
-			
-		// clean up WordPress dashboard with this CSS 
-		function minimal_dashboard_custom_admin_styles() {
-		echo '<style type="text/css">	
-					.wp-menu-separator	{ display:none; }
-					#wp-admin-bar-comments	{ display:none; } 
-					#wp-admin-bar-new-content	{ display:none; } 
-					#wp-admin-bar-wpseo-menu	{ display:none; }
-					#footer	{ display:none; }  
-					.tablenav.top	{ display:none; }
-					#screen-options-link-wrap	{ display:none; }
-					#contextual-help-link-wrap	{ display:none; }
-					.column-wpseo-score	{ display:none; }
-					.column-wpseo-title	{ display:none; }
-					.column-wpseo-metadesc	{ display:none; }
-					.column-wpseo-focuskw	{ display:none; }
-					#collapse-menu	{ display:none; }
-					</style>';
-		}
-			
-			
-		// hide menu items from all users 
-		function minimal_dashboard_remove_menu_items() {
-		  global $menu;
-		  $restricted = array(__('Links'), __('Comments'), __('Media'), __('Dashboard'), __('Tools'), __('Profile'));
-		  end ($menu);
-			  while (prev($menu)){
-			    $value = explode(' ',$menu[key($menu)][0]);
-			    if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){
-			      unset($menu[key($menu)]);}
-			  }
-		}
-		add_action('admin_menu', 'minimal_dashboard_remove_menu_items');
-			
-			
-		// tidy up edit page screen to leave just the title 
-		function minimal_dashboard_custom_columns($defaults) {
-		  unset($defaults['comments']);
-		  unset($defaults['author']);
-		  unset($defaults['date']);
-		  unset($defaults['categories']);
-		  unset($defaults['tags']);
-		  unset($defaults['wpseo-score']); 
-		  return $defaults;
-		}
-		add_filter('manage_pages_columns', 'minimal_dashboard_custom_columns');
-		add_filter('manage_posts_columns', 'minimal_dashboard_custom_columns');
-			
-			
-		// hide dashboard by redirecting user to 'all pages' 
-		function minimal_dashboard_hide_dashboard () {
-		        if ( preg_match( '#wp-admin/?(index.php)?$#', $_SERVER['REQUEST_URI'] )) {
-		                wp_redirect( get_option( 'siteurl' ) . '/wp-admin/edit.php?post_type=page');
-		        }
-		}
-		add_action('admin_menu', 'minimal_dashboard_hide_dashboard');
-			
-			
-		// grant editor access to gravity forms 
-		function minimal_dashboard_add_grav_forms(){
-			$role = get_role('editor');
-			$role->add_cap('gform_full_access');
-		}
-		add_action('admin_init','minimal_dashboard_add_grav_forms');
+		add_filter( 'show_admin_bar', '__return_false' );
+
+		add_action( 'admin_menu', array( &$this, 'minimal_dashboard_remove_menu_items' ) );
+
+		add_filter( 'manage_pages_columns', array( &$this, 'minimal_dashboard_custom_columns' ) );
+		add_filter( 'manage_posts_columns', array( &$this, 'minimal_dashboard_custom_columns' ) );
+
+		add_action( 'admin_menu', array( &$this, 'minimal_dashboard_hide_dashboard' ) );
+
+		add_action( 'admin_init', array( &$this, 'minimal_dashboard_add_grav_forms' ) );
+	}
+		
+	// clean up WordPress dashboard with this CSS 
+	function minimal_dashboard_custom_admin_styles() {
+	echo '<style type="text/css">	
+				.wp-menu-separator	{ display:none; }
+				#wp-admin-bar-comments	{ display:none; } 
+				#wp-admin-bar-new-content	{ display:none; } 
+				#wp-admin-bar-wpseo-menu	{ display:none; }
+				#footer	{ display:none; }  
+				.tablenav.top	{ display:none; }
+				#screen-options-link-wrap	{ display:none; }
+				#contextual-help-link-wrap	{ display:none; }
+				.column-wpseo-score	{ display:none; }
+				.column-wpseo-title	{ display:none; }
+				.column-wpseo-metadesc	{ display:none; }
+				.column-wpseo-focuskw	{ display:none; }
+				#collapse-menu	{ display:none; }
+				</style>';
+	}
+		
+		
+	// hide menu items from all users 
+	function minimal_dashboard_remove_menu_items() {
+	  global $menu;
+	  $restricted = array(__('Links'), __('Comments'), __('Media'), __('Dashboard'), __('Tools'), __('Profile'));
+	  end ($menu);
+		  while (prev($menu)){
+		    $value = explode(' ',$menu[key($menu)][0]);
+		    if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){
+		      unset($menu[key($menu)]);}
+		  }
+	}
+		
+		
+	// tidy up edit page screen to leave just the title 
+	function minimal_dashboard_custom_columns($defaults) {
+	  unset($defaults['comments']);
+	  unset($defaults['author']);
+	  unset($defaults['date']);
+	  unset($defaults['categories']);
+	  unset($defaults['tags']);
+	  unset($defaults['wpseo-score']); 
+	  return $defaults;
+	}
+		
+		
+	// hide dashboard by redirecting user to 'all pages' 
+	function minimal_dashboard_hide_dashboard () {
+	        if ( preg_match( '#wp-admin/?(index.php)?$#', $_SERVER['REQUEST_URI'] )) {
+	                wp_redirect( get_option( 'siteurl' ) . '/wp-admin/edit.php?post_type=page');
+	        }
+	}
+		
+		
+	// grant editor access to gravity forms 
+	function minimal_dashboard_add_grav_forms(){
+		$role = get_role('editor');
+		$role->add_cap('gform_full_access');
 	}
 }
 
